80ceb2bb65cf770cf0967adc9003a44292a7ab09
Verma, Shubham <60651088+shubhamrly@users.noreply.github.com>
2025-07-31 20:02:27 +0530

    Refactor: for readbilty fixed some logs

--- src/main/java/com/momentum/minimomentum/advice/GlobalControllerAdvice.java
+++ src/main/java/com/momentum/minimomentum/advice/GlobalControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.momentum.minimomentum.advice;
 
 import com.momentum.minimomentum.exception.PromptNotFoundException;
+import com.momentum.minimomentum.exception.SummaryNotFoundException;
+import com.momentum.minimomentum.exception.TranscriptNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +14,18 @@ public class GlobalControllerAdvice {
         return ResponseEntity
                 .badRequest()
                 .body("Prompt not found: " + e.getMessage());
+    }
+    @ExceptionHandler(SummaryNotFoundException.class)
+    public ResponseEntity<String> handleSummaryNotFoundException(Exception e) {
+        return ResponseEntity
+                .badRequest()
+                .body("Summary not found: " + e.getMessage());
+    }
+
+    @ExceptionHandler(TranscriptNotFoundException.class)
+    public ResponseEntity<String> handleTranscriptNotFoundException(Exception e) {
+        return ResponseEntity
+                .badRequest()
+                .body("Transcript not found: " + e.getMessage());
     }
 }
